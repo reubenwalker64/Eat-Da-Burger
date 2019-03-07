c399b857a18a8b075ad428351d902cb94aa154dc
reubenwalker64 <rwalker@mobile-atom.com>
2019-03-07 15:38:13 -0500

    seeds schema and index handlebars options

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Buffalo Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Emu Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Incredible Burger', false);
