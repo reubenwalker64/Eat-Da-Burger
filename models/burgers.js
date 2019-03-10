314613c46a1c574a1f52d7ad20f75f16ae8b8c5b
reubenwalker64 <rwalker@mobile-atom.com>
2019-03-10 14:41:24 -0400

    server js edits

--- models/burgers.js
+++ models/burgers.js
@@ -8,21 +8,17 @@ var burger = {
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function(name, cb) {
+    orm.create("burgers", name, cb function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  update: function(id, cb) {
+    orm.update("burgers", id, cb function(res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+  
 };
 
 // Export the database functions for the controller (catsController.js).
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
+//need body parser???
+var bodyParser = require("body-parser");
+var routes = require("./controllers/burgers_controller.js");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Need body parser???
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use("/", routes);
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-//Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
