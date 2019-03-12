750ec2bd1fb5b0f40f46f1dc345ed68ef43fcc3a
reubenwalker64 <rwalker@mobile-atom.com>
2019-03-12 18:00:13 -0400

    heroku setup

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+if (process.env.mysql://a4hmt6bb4q7skb4k:hjtfuo5rftp15epg@s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r6z0ept2lvch3euu) {
+  connection = mysql.createConnection(process.env.mysql://a4hmt6bb4q7skb4k:hjtfuo5rftp15epg@s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/r6z0ept2lvch3euu);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "punker64",
+    database: "burgers_db"
+  })
+}
+/*var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "punker64",
   database: "burgers_db"
 });
-
+*/
 // Make connection.
 connection.connect(function(err) {
   if (err) {
